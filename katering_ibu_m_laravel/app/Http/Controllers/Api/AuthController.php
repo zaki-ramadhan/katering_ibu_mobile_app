f7c1d7acc4e4afc6dd73c00c2e6be0eb0a849f71
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-04-06 01:45:27 +0700

    Instalasi sanctum api, test api di postman, membuat authController, modifikasi model User

---
+++ katering_ibu_m_laravel/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token]);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logged out']);
+    }
+}
--- katering_ibu_m_laravel/config/auth.php
+++ katering_ibu_m_laravel/config/auth.php
@@ -36,6 +36,10 @@ return [
     */
 
     'guards' => [
+        // 'api' => [
+        //     'driver' => 'sanctum',
+        //     'provider' => 'users',
+        // ],
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
